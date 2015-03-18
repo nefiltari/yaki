03a34187af08aefbfe6bf2825c40d35e0ad16db8
Francesco Möller <francesco.moeller@leansky.eu>
2015-03-18 03:19:59 +0100

    Changed Version to 0.0.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nefiltari:yaki',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'Yaki can capture relevant tags from any bunch of text.',
   git: 'https://github.com/nefiltari/yaki.git',
   documentation: 'README.md'
